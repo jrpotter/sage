0a57723ead601954843a14a81f0524cbc6d02b26
Joshua Potter <jrpotter2112@gmail.com>
2015-12-05 20:10:22 -0500

    AST nodes

---
+++ src/ast.cpp
@@ -0,0 +1,18 @@
+/**
+ * ast.cpp
+ *
+ * Created by jrpotter (12/05/2015).
+ */
+
+#include "ast.h"
+
+using namespace sage;
+
+
+/**
+ * Constructor
+ * ================================
+ */
+AST::AST(std::string symbol)
+    : symbol(symbol)
+{ }
