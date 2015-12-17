bb6c2efc0576de1e2e4f038bc74cfec53277d95f
Joshua Potter <jrpotter2112@gmail.com>
2015-12-17 15:48:01 -0500

    removal of main content

--- main.cpp
+++ main.cpp
@@ -8,20 +8,6 @@ using namespace sage;
 
 int main(int argc, char** argv)
 {
-    //Parser parser("/home/jrpotter/Documents/sage/grammars/arithmetic.peg");
-    //std::stringstream test("195 + (186 * 32) - 14 / 9");
-
-    Parser parser("/home/jrpotter/Documents/sage/grammars/palindrome.peg");
-    std::stringstream test("abacab     a");
-
-    auto tree = parser.parse(test);
-    if(tree) {
-        std::stringstream ss;
-        tree->format(ss);
-        std::cout << ss.str() << std::endl;
-    } else {
-        std::cout << "failure" << std::endl;
-    }
 
     return 0;
 }
